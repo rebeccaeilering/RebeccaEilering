959842fadd770f3bc0ab4336329a7968de0a7d8a
Rebecca Eilering <rebeccaeilering@gmail.com>
2018-02-17 21:45:21 -0600

    added subpage template and styles

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
 
   // Mobile Navigation
+  // Open Nav
   $('#mobile-nav').click(function() {
     $('nav').addClass('open')
   });
-
+  // Close Nav
   $('#mobile-close').click(function() {
       $('nav').removeClass('open');
   });
