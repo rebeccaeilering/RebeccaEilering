9418548e8e238f359c95ebbc1c346c5ab03d4d74
Rebecca Eilering <Rebecca.Eilering@lrs.com>
2020-06-05 14:37:00 -0500

    added active class for the nav

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,49 +2,63 @@
 window.sr = ScrollReveal();
 sr.reveal('.boom');
 
-//Mobile Nav Open/Close
-const mobileNav = document.getElementById('mobile-nav');
-const nav = document.querySelector('nav');
-const mobileClose = document.getElementById('mobile-close')
+"use strict";
+document.addEventListener("DOMContentLoaded", function () {
 
-mobileNav.addEventListener('click', function() {
-  nav.classList.add('open');
-  this.classList.add('hide');
-});
+  // Add active class to nav item
 
-mobileClose.addEventListener('click', function() {
-  nav.classList.remove('open');
-  mobileNav.classList.remove('hide');
-});
-
-// Mobile Nav Toggler
-const hasChildren = document.querySelectorAll('.has-children');
-let num1 = 0;
-hasChildren.forEach(child => {
-  num1++;
-  child.innerHTML += ` <button id="nav-toggle-${num1}"><i class="fas fa-plus-circle"></i></button>`;
-});
-
-const navBtns = document.querySelectorAll('nav button');
-navBtns.forEach(button => {
-  const num = button.getAttribute('id').split('-').pop();
-  const slSection = document.getElementById(`sl-${num}`);
-
-  button.addEventListener('click', () => {
-    button.classList.toggle('expanded');
-    slSection.classList.toggle('open');
-    if (button.classList.contains('expanded')) {
-      button.innerHTML = `<i class="fas fa-minus-circle"></i>`;
-    } else {
-      button.innerHTML = `<i class="fas fa-plus-circle"></i>`;
-    }
+  const current = location.pathname.split('/')[1];
+  if (current === '') return;
+  const menuItems = document.querySelectorAll('nav .a1');
+  menuItems.forEach(item => {
+    if (item.getAttribute('href').indexOf(current) !== -1) {
+      item.classList.add('active');
+    };
   });
 
-});
 
-// Responsive Tables
-const tables = document.querySelector('table');
-if (tables) {
+  //Mobile Nav Open/Close
+  const mobileNav = document.getElementById('mobile-nav');
+  const nav = document.querySelector('nav');
+  const mobileClose = document.getElementById('mobile-close')
+
+  mobileNav.addEventListener('click', function() {
+    nav.classList.add('open');
+    this.classList.add('hide');
+  });
+
+  mobileClose.addEventListener('click', function() {
+    nav.classList.remove('open');
+    mobileNav.classList.remove('hide');
+  });
+
+  // Mobile Nav Toggler
+  const hasChildren = document.querySelectorAll('.has-children');
+  let num1 = 0;
+  hasChildren.forEach(child => {
+    num1++;
+    child.innerHTML += ` <button id="nav-toggle-${num1}"><i class="fas fa-plus-circle"></i></button>`;
+  });
+
+  const navBtns = document.querySelectorAll('nav button');
+  navBtns.forEach(button => {
+    const num = button.getAttribute('id').split('-').pop();
+    const slSection = document.getElementById(`sl-${num}`);
+
+    button.addEventListener('click', () => {
+      button.classList.toggle('expanded');
+      slSection.classList.toggle('open');
+      if (button.classList.contains('expanded')) {
+        button.innerHTML = `<i class="fas fa-minus-circle"></i>`;
+      } else {
+        button.innerHTML = `<i class="fas fa-plus-circle"></i>`;
+      }
+    });
+
+  });
+
+  // Responsive Tables
+  const tables = document.querySelector('table');
   const headertext = [],
   headers = document.querySelectorAll("table th"),
   tablerows = document.querySelectorAll("table th"),
@@ -59,12 +73,10 @@ if (tables) {
       col.setAttribute("data-th", headertext[j]);
     } 
   }
-}
 
-// Accordions
-const accordions = document.querySelectorAll('.accordions');
-let num = 0;
-if (accordions) {
+  // Accordions
+  const accordions = document.querySelectorAll('.accordions');
+  let num = 0;
   accordions.forEach(accordions => {
     num++;
     accordions.setAttribute('id', `accordionGroup-${num}`);
@@ -87,83 +99,83 @@ if (accordions) {
       }
     });
   });
-}
 
-// Tabs
-const tabs = document.querySelectorAll('.tabs-container');
-if(tabs) {
-  class TabController {
-    constructor(container) {
-      this.container = document.querySelector(container);
-      this.tablist = this.container.querySelector('[role=tablist]');
-      this.tabs = this.container.querySelectorAll('[role=tab]');
-      this.tabpanels = this.container.querySelectorAll('[role=tabpanel]');
-      this.activeTab = this.container.querySelector('[role=tab][aria-selected=true]');
-  
-      this._addEventListeners();
-    }
-  
-    // Private function to set event listeners
-    _addEventListeners() {
-      for (let tab of this.tabs) {
-        tab.addEventListener('click', e => {
-          e.preventDefault();
-          this.setActiveTab(tab.getAttribute('aria-controls'));
-        });
-        tab.addEventListener('keyup', e => {
-          if (e.keyCode == 13 || e.keyCode == 32) { // return or space
+  // Tabs
+  const tabs = document.querySelectorAll('.tabs-container');
+  if(tabs) {
+    class TabController {
+      constructor(container) {
+        this.container = document.querySelector(container);
+        this.tablist = this.container.querySelector('[role=tablist]');
+        this.tabs = this.container.querySelectorAll('[role=tab]');
+        this.tabpanels = this.container.querySelectorAll('[role=tabpanel]');
+        this.activeTab = this.container.querySelector('[role=tab][aria-selected=true]');
+    
+        this._addEventListeners();
+      }
+    
+      // Private function to set event listeners
+      _addEventListeners() {
+        for (let tab of this.tabs) {
+          tab.addEventListener('click', e => {
             e.preventDefault();
             this.setActiveTab(tab.getAttribute('aria-controls'));
+          });
+          tab.addEventListener('keyup', e => {
+            if (e.keyCode == 13 || e.keyCode == 32) { // return or space
+              e.preventDefault();
+              this.setActiveTab(tab.getAttribute('aria-controls'));
+            }
+          })
+        }
+        this.tablist.addEventListener('keyup', e => {
+          switch (e.keyCode) {
+            case 35: // end key
+              e.preventDefault();
+              this.setActiveTab(this.tabs[this.tabs.length - 1].getAttribute('aria-controls'));
+              break;
+            case 36: // home key
+              e.preventDefault();
+              this.setActiveTab(this.tabs[0].getAttribute('aria-controls'));
+              break;
+            case 37: // left arrow
+              e.preventDefault();
+              let previous = [...this.tabs].indexOf(this.activeTab) - 1;
+              previous = previous >= 0 ? previous : this.tabs.length - 1;
+              this.setActiveTab(this.tabs[previous].getAttribute('aria-controls'));
+              break;
+            case 39: // right arrow
+              e.preventDefault();
+              let next = [...this.tabs].indexOf(this.activeTab) + 1;
+              next = next < this.tabs.length ? next : 0
+              this.setActiveTab(this.tabs[next].getAttribute('aria-controls'));
+              break;
           }
         })
       }
-      this.tablist.addEventListener('keyup', e => {
-        switch (e.keyCode) {
-          case 35: // end key
-            e.preventDefault();
-            this.setActiveTab(this.tabs[this.tabs.length - 1].getAttribute('aria-controls'));
-            break;
-          case 36: // home key
-            e.preventDefault();
-            this.setActiveTab(this.tabs[0].getAttribute('aria-controls'));
-            break;
-          case 37: // left arrow
-            e.preventDefault();
-            let previous = [...this.tabs].indexOf(this.activeTab) - 1;
-            previous = previous >= 0 ? previous : this.tabs.length - 1;
-            this.setActiveTab(this.tabs[previous].getAttribute('aria-controls'));
-            break;
-          case 39: // right arrow
-            e.preventDefault();
-            let next = [...this.tabs].indexOf(this.activeTab) + 1;
-            next = next < this.tabs.length ? next : 0
-            this.setActiveTab(this.tabs[next].getAttribute('aria-controls'));
-            break;
+    
+      // Public function to set the tab by id
+      setActiveTab(id) {
+        for (let tab of this.tabs) {
+          if (tab.getAttribute('aria-controls') == id) {
+            tab.setAttribute('aria-selected', "true");
+            tab.focus();
+            this.activeTab = tab;
+          } else {
+            tab.setAttribute('aria-selected', "false");
+          }
         }
-      })
-    }
-  
-    // Public function to set the tab by id
-    setActiveTab(id) {
-      for (let tab of this.tabs) {
-        if (tab.getAttribute('aria-controls') == id) {
-          tab.setAttribute('aria-selected', "true");
-          tab.focus();
-          this.activeTab = tab;
-        } else {
-          tab.setAttribute('aria-selected', "false");
-        }
-      }
-      for (let tabpanel of this.tabpanels) {
-        if (tabpanel.getAttribute('id') == id) {
-          tabpanel.setAttribute('aria-expanded', "true");
-        } else {
-          tabpanel.setAttribute('aria-expanded', "false");
+        for (let tabpanel of this.tabpanels) {
+          if (tabpanel.getAttribute('id') == id) {
+            tabpanel.setAttribute('aria-expanded', "true");
+          } else {
+            tabpanel.setAttribute('aria-expanded', "false");
+          }
         }
       }
     }
+    
+    const tabController = new TabController('#tabs');
   }
-  
-  const tabController = new TabController('#tabs');
-}
 
+});
